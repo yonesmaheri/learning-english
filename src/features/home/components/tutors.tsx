f6a360796b01d331a349d0339955013980e10096
you <you@example.com>
2026-05-29 11:48:14 +0330

    ..

--- src/features/home/components/tutors.tsx
+++ src/features/home/components/tutors.tsx
@@ -6,12 +6,6 @@ function Tutors({ data }: { data: any[] }) {
     <div dir="rtl">
       <div className="w-full flex items-center justify-between mt-10">
         <h2 className="font-bold text-xl lg:text-2xl">اساتید منتخب</h2>
-        <Link
-          href={`/tutor`}
-          className="text-xs  text-gray-400 transition-all duration-300 group-hover:text-indigo-500 group-hover:border-indigo-500 border px-4 py-2.5 rounded-xl border-gray-400"
-        >
-          همه مدرسان
-        </Link>
       </div>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 mt-4">
         {data.slice(0, 3).map((tutor) => (
